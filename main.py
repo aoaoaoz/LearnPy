996660d9d0a481c2e0e53058259c7ddbd39ba218
Night <1060390387@qq.com>
2020-03-03 21:54:31 +0800

    practice python

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-import math
 
-def quadratic(a, b, c):
-    delta = b*b-4*a*c
-    if delta < 0 : return
-    delta = math.sqrt(delta)
-    return (-b+delta)/(2*a), (-b-delta)/(2*a)
-print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
-print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
-if quadratic(2, 3, 1) != (-0.5, -1.0):
-    print('测试失败')
-elif quadratic(1, 3, -4) != (1.0, -4.0):
-    print('测试失败')
-else:
-    print('测试成功')
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+    return t[1]
+    pass
+L2 = sorted(L, key = by_name, reverse = True)
+print(L2)
