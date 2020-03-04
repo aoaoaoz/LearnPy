01f2610148720857578571c59b82e55109c91578
Night <1060390387@qq.com>
2020-03-04 22:38:14 +0800

    learn @property

--- main.py
+++ main.py
@@ -3,15 +3,34 @@
 ' a test module '
 __author__ = 'aoaoao'
 
-import sys
-
-def test():
-    args = sys.argv
-    if len(args) == 1:
-        print('Hello, world!')
-    elif len(args) == 2:
-        print('Hello, %s' % args[1])
-    else : print('Too many arguments')
-
-if __name__ == '__main__':
-    test()
+class Screen(object):
+    
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self, x):
+        if not isinstance(x, int):
+            raise TypeError('sdf')
+        self._width = x
+    @property
+    def height(self):
+        return self._height
+    
+    @width.setter
+    def height(self, x):
+        if not isinstance(x, int):
+            raise TypeError('sdf')
+        self._height = x
+    @property
+    def resolution(self):
+        return self._width * self._height
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
