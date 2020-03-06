5b9a9b44070c1f5db915cd4f224f668eef2fa9c0
Night <1060390387@qq.com>
2020-03-06 20:04:40 +0800

    queue and pipe

--- main.py
+++ main.py
@@ -3,22 +3,27 @@
 ' a test module '
 __author__ = 'aoaoao'
 
+from multiprocessing import Process, Queue
 import os, time, random
-from multiprocessing import Pool
 
-def long_time_task(name):
-    print('Run task %s (%s)...' % (name, os.getpid()))
-    start = time.time()
-    time.sleep(random.random() * 3)
-    end = time.time()
-    print('Task %s run %0.2f seconds.' % (name, (end-start)))
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
 
 if __name__ == '__main__':
-    print ('Parent process %s.' % os.getpid())
-    p = Pool(4)
-    for i in range(5):
-        p.apply_async(long_time_task, args=(i,))
-    print('Waiting for all subprocesses done...')
-    p.close()
-    p.join()
-    print('All subprocesses done.')
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    pw.start()
+    pr.start()
+    pw.join()
+    pr.terminate()
