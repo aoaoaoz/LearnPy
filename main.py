d777bbfd05cf160e1ac5fe6a6d01f203c0b3f112
Night <1060390387@qq.com>
2020-03-07 14:04:33 +0800

    datetime

--- main.py
+++ main.py
@@ -4,11 +4,21 @@
 __author__ = 'aoaoao'
 
 import re
+from datetime import datetime, timezone, timedelta
+def to_timestamp(dt_str, tz_str):
+    cday = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
+    reg = re.compile(r'^UTC([\+\-])(\d+)\:00$')
+    x = reg.match(tz_str)
+    if x.group(1)=='-':
+        num = -int(x.group(2))
+    else:
+        num = int(x.group(2))
+    dt = cday.replace(tzinfo=timezone(timedelta(hours=num)))
+    return dt.timestamp()
+t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+assert t1 == 1433121030.0, t1
 
-def name_of_email(addr):
-    reg = re.compile(r'^\<*([\w\s]*)\>*([\s\w]*)@(\w+)\.org$')
-    return reg.match(addr).group(1)
+t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
+assert t2 == 1433121030.0, t2
 
-assert name_of_email('<Tom Paris> tom@voyager.org') == 'Tom Paris'
-assert name_of_email('tom@voyager.org') == 'tom'
 print('ok')
