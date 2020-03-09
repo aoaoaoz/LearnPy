320fffc4a4d8c96d143c81109790b0263cd65b1c
Night <1060390387@qq.com>
2020-03-09 20:42:15 +0800

    learn numpy

--- main.py
+++ main.py
@@ -3,32 +3,7 @@
 ' a test module '
 __author__ = 'aoaoao'
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    body = '<h1>Hello, %s</h1>' % (environ['PATH_INFO'][1:] or 'web')
-    return [body.encode('utf-8')]
+import numpy as np
 
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/', methods = ['GET', 'POST'])
-def home():
-    return '<h1>Home</h1>'
-
-@app.route('/signin', methods = ['GET'])
-def signin_form():
-    return '''<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>'''
-
-@app.route('/signin', methods=['POST'])
-def signin():
-    if request.form['username']=='admin' and request.form['password']=='password':
-        return '<h3>Hello, admin!</h3>'
-    return '<h3>Bad username or password.</h3>'
-
-if __name__ == '__main__':
-    app.run()
+a = [1.43, 23.342, 2.64, 254.352]
+print( np.around( a, decimals = 1 ) )
